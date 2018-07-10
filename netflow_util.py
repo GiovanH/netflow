5733395ac329287d7ab5a6f7167e7d3bcf879fa8
Gio <gio@utdallas.edu>
2018-07-09 21:37:49 -0500

    More PEP8 compliance

--- netflow_util.py
+++ netflow_util.py
@@ -1,15 +1,17 @@
 #!/bin/python3
-#Seth Giovanetti
+# Seth Giovanetti
 
 import pickle
 import os
 
-def simple_combine_data(data,sortField):
-    return combine_data(data, lambda a,b: a[sortField]==b[sortField], sortField)
+
+def simple_combine_data(data, sortField):
+    return combine_data(data, lambda a, b: a[sortField] == b[sortField], sortField)
+
 
 def combine_data(data, equals, sortField):
-    #Combines records in data based on the equals function.
-    #Only sequential entries after sorting by sortfield will be combined.
+    # Combines records in data based on the equals function.
+    # Only sequential entries after sorting by sortfield will be combined.
     if len(data) is 0:
         print("Warning! Data is empty!")
         return data
@@ -36,40 +38,42 @@ def combine_data(data, equals, sortField):
     groups.append(group)
     finaldata = []
     for l in groups:
-            combined = {}
-            for key in l[0].keys():
-                    value = 0
-                    for entry in l:
-                            try:
-                                    value += entry[key]
-                            except TypeError:
-                                    value = entry[key]
-                                    break
-                    combined[key] = value
-            finaldata.append(combined)
+        combined = {}
+        for key in l[0].keys():
+            value = 0
+            for entry in l:
+                try:
+                    value += entry[key]
+                except TypeError:
+                    value = entry[key]
+                    break
+            combined[key] = value
+        finaldata.append(combined)
     return finaldata
+
 
 def top_percent(predata, percent, field, total):
     totalrecords = len(predata)
-        
-    #Get top 10 records by bytes_in
-    #Sorts the list by field (bytes_in), gets the #n... #2, #1 entries.
+
+    # Get top 10 records by bytes_in
+    # Sorts the list by field (bytes_in), gets the #n... #2, #1 entries.
     predata = sorted(predata, key=lambda k: k[field])
-    
-    #Initialize array for the final data points
+
+    # Initialize array for the final data points
     data = []
-    #Map included/total ratio while adding
+    # Map included/total ratio while adding
     included = 0
-    
-    #Calculate top % of data, and use that as the data we graph
-    #Until the amount of data we have exceeds %[percent], move a data point into our final list.
+
+    # Calculate top % of data, and use that as the data we graph
+    # Until the amount of data we have exceeds %[percent], move a data point into our final list.
     while len(predata) > 0 and included < total*(percent/100):
         newrecord = predata.pop()
         included += newrecord[field]
         data.append(newrecord)
     print("Records included: " + str(len(data)) + '/' + str(totalrecords))
     return data
-    
+
+
 def pickleLoad(filename):
     """
     TODO: Docstring
@@ -83,6 +87,7 @@ def pickleLoad(filename):
     filehandler = open(path(filename), 'rb')
     object = pickle.load(filehandler)
     return object
+
 
 def pickleSave(object, filename):
     """
@@ -104,16 +109,18 @@ def pickleSave(object, filename):
     inst.dump(object)
     inst.clear_memo()
 
+
 def path(file):
-    return "obj/" + file.replace(".","_").replace("\\","_") + ".obj"
+    return "obj/" + file.replace(".", "_").replace("\\", "_") + ".obj"
+
 
 def sluggify(value):
     """
     Normalizes string, converts to lowercase, removes non-alpha characters,
     and converts spaces to hyphens.
     """
-    r = value.replace("..\\","prev")
-    r = r.replace("\"","").replace(" ","_")
-    r = r.replace("\\","_").replace("/","_")
-    r = r.replace("*","+").replace("%","perc")
+    r = value.replace("..\\", "prev")
+    r = r.replace("\"", "").replace(" ", "_")
+    r = r.replace("\\", "_").replace("/", "_")
+    r = r.replace("*", "+").replace("%", "perc")
     return r
