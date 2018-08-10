1c77e23d38b405e72874be21868fb9c4fdd90492
Seth <gio@utdallas.edu>
2018-08-09 21:59:24 -0500

    compatibility

--- netflow_csv.py
+++ netflow_csv.py
@@ -9,7 +9,10 @@ import sys
 
 def opencsv(globstr, cap):
     data = []
-    for filename in glob.glob(globstr):
+    # print(globstr)
+    filenames = glob.glob(globstr)
+    # print(filenames)
+    for filename in filenames:
         print(filename)
         sys.stdout.flush()
         print("Reading file")
@@ -20,19 +23,25 @@ def opencsv(globstr, cap):
             sheet = []
             i = 1
             for row in reader:
-                i += 1
-                row['filename'] = filename
-                row['linenum'] = str(i)
-                row['bytes_in'] = int(row['bytes_in'])
-                row['time'] = row['_time'][11:13]
+                try:
+                    i += 1
+                    row['filename'] = filename
+                    row['linenum'] = str(i)
+                    row['bytes_in'] = int(row['bytes_in'])
+                    row['time'] = row['_time'][11:13]
 
-                # Let's only read the fields we're interested in, to save time.
-                sheet.append({field: row[field] for field in ['bytes_in', 'dest_ip', 'flow_dir', 'src_ip', 'linenum', 'time', 'filename']})
-                if (i == cap):
+                    # Let's only read the fields we're interested in, to save time.
+                    sheet.append({field: row[field] for field in ['bytes_in', 'dest_ip', 'flow_dir', 'src_ip', 'linenum', 'time', 'filename']})
+                    if (i == cap):
+                        break
+                    if (i % 50000 == 0):
+                        print('#', end='')
+                        sys.stdout.flush()
+                except:
+                    print("Row error on file " + filename + " row " + str(i))
+                    print(row)
+                    print("Skipping file.")
                     break
-                if (i % 50000 == 0):
-                    print('#', end='')
-                    sys.stdout.flush()
         print(']')
         sys.stdout.flush()
         sheet = util.multi_combine_data(sheet, ['dest_ip', 'flow_dir', 'src_ip', 'time', 'filename'])
