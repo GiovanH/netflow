4b2fdd63ae9041daa3874e835f7e1aa3ae33d767
Seth <gio@utdallas.edu>
2018-09-14 22:28:51 -0500

    Updated get script to only get the recent files and keep storage costs managable.
    Also updated the tests

--- get.sh
+++ get.sh
@@ -1,2 +1,9 @@
 # scp -rp csnetflowdata:/data/netflow/ /data/netflow/
-rsync -avP --delete-before --stats csnfd:/data/netflow/ /data/netflow/ | tee ~/logs/lastget.log
+max_storage_days=7
+
+#Delete outdated files
+find /data/netflow/ -mtime +$max_storage_days -exec rm -rv {} \;
+
+#Get new files
+ssh -qx csnfd "find /data/netflow/* -type d -mtime -$max_storage_days -print0" | \
+    rsync --from0 --files-from=- -riv "csnfd:/" /data/netflow/
